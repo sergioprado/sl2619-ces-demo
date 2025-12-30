86aa559e00fb1b0d951767830a7ca07e71ea1609
Sergio Prado <sergio.prado@e-labworks.com>
2025-12-30 05:59:02 -0300

    build.sh: make docker user configurable
    Signed-off-by: Sergio Prado <sergio.prado@e-labworks.com>

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-DOUSER="sergioprado"
+DOUSER="$1"
 DOTAG="subtitle-demo"
 DOARCH="arm64"
 DOVERSION="${DOARCH}-1.0"
+
+if [ -z "$DOUSER" ]; then
+    DOUSER="sergioprado"
+fi
 
 docker buildx build --platform linux/${DOARCH} -t ${DOUSER}/${DOTAG}:${DOVERSION} .
 docker push ${DOUSER}/${DOTAG}:${DOVERSION}
